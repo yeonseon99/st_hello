f3f35d79b087a5cf3450418996acdae4441a425f
yeonseon99 <109208184+yeonseon99@users.noreply.github.com>
2023-04-26 15:33:19 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -2,13 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC 우버 픽업 예제')
-
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
+st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
